b70700874a221d233e502e868d09451ebb0fe839
theskyworld <2456450687@qq.com>
2024-01-12 01:43:14 +0800

    feat: add WatchHistoryPart

---
+++ components/WatchHistoryPart/index.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const watchedVideos = ref([
+  {
+    id: 1,
+    imgURL: "https://m.ykimg.com/0541010160F63EBA04CD829CF4BB9E9C",
+    title: "神话12",
+    watchTime: new Date(),
+    watchPercent: 12,
+  },
+  {
+    id: 2,
+    imgURL: "https://m.ykimg.com/0541010160F63EBA04CD829CF4BB9E9C",
+    title: "神话12",
+    watchTime: new Date(),
+    watchPercent: 12,
+  },
+  {
+    id: 3,
+    imgURL: "https://m.ykimg.com/0541010160F63EBA04CD829CF4BB9E9C",
+    title: "神话12",
+    watchTime: new Date(),
+    watchPercent: 12,
+  },
+  {
+    id: 4,
+    imgURL: "https://m.ykimg.com/0541010160F63EBA04CD829CF4BB9E9C",
+    title: "神话12",
+    watchTime: new Date(),
+    watchPercent: 12,
+  },
+  {
+    id: 5,
+    imgURL: "https://m.ykimg.com/0541010160F63EBA04CD829CF4BB9E9C",
+    title: "神话12",
+    watchTime: new Date(),
+    watchPercent: 12,
+  },
+]);
+</script>
+<template>
+  <div class="watch-history-container">
+    <div class="left-line"></div>
+    <div class="right-content-wrapper">
+      <div class="title-wrapper">
+        <i class="iconfont icon-history"></i>
+        <h5>观看历史</h5>
+      </div>
+      <div class="main-content-wrapper">
+        <ul v-if="watchedVideos.length">
+          <li
+            v-for="{
+              id,
+              imgURL,
+              title,
+              watchTime,
+              watchPercent,
+            } in watchedVideos"
+            :key="id"
+          >
+            <BaseWatchHistoryPartCard
+              :img-URL="imgURL"
+              :title="title"
+              :watch-time="watchTime"
+              :watch-percent="watchPercent"
+            />
+          </li>
+        </ul>
+        <div class="no-video" v-else>暂无记录</div>
+      </div>
+    </div>
+    <div class="bottom-btn-wrapper">
+      <button v-if="watchedVideos.length">查看更多</button>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+@use "./watchHistoryPart.scss";
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,60 +2,11 @@
 useHead({
   title: "ABCD影视",
 });
-const watchedVideos = ref([
-  {
-    id: 1,
-    imgURL: "https://m.ykimg.com/0541010160F63EBA04CD829CF4BB9E9C",
-    title: "神话12",
-    watchTime: new Date(),
-    watchPercent: 12,
-  },
-  {
-    id: 2,
-    imgURL: "https://m.ykimg.com/0541010160F63EBA04CD829CF4BB9E9C",
-    title: "神话12",
-    watchTime: new Date(),
-    watchPercent: 12,
-  },
-  {
-    id: 3,
-    imgURL: "https://m.ykimg.com/0541010160F63EBA04CD829CF4BB9E9C",
-    title: "神话12",
-    watchTime: new Date(),
-    watchPercent: 12,
-  },
-  {
-    id: 4,
-    imgURL: "https://m.ykimg.com/0541010160F63EBA04CD829CF4BB9E9C",
-    title: "神话12",
-    watchTime: new Date(),
-    watchPercent: 12,
-  },
-  {
-    id: 5,
-    imgURL: "https://m.ykimg.com/0541010160F63EBA04CD829CF4BB9E9C",
-    title: "神话12",
-    watchTime: new Date(),
-    watchPercent: 12,
-  },
-]);
 </script>
 <template>
   <div>首页</div>
   <div>
-    <ul v-if="watchedVideos.length">
-      <li
-        v-for="{ id, imgURL, title, watchTime, watchPercent } in watchedVideos"
-        :key="id"
-      >
-        <BaseWatchHistoryPartCard
-          :img-URL="imgURL"
-          :title="title"
-          :watch-time="watchTime"
-          :watch-percent="watchPercent"
-        />
-      </li>
-    </ul>
+    <WatchHistoryPart />
   </div>
 </template>
 <style scoped></style>
