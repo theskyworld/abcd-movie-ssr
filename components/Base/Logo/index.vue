57ce2f1dcf71cbbbc8a876ea65ee58a841bee766
theskyworld <2456450687@qq.com>
2024-01-11 21:14:42 +0800

    feat: add Logo

---
+++ components/Base/Logo/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="logo-container">
+    <h1>
+      <img src="~/assets/imgs/ABCD影视.svg" alt="ABCD影视" />
+    </h1>
+  </div>
+</template>
+<style scoped lang="scss">
+@use "./logo.scss";
+</style>
