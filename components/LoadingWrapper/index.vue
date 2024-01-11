1b36d23e5dece598c4f04a9091739ed607d93377
theskyworld <2456450687@qq.com>
2024-01-12 04:06:42 +0800

    feat: add LoadingWrapper

---
+++ components/LoadingWrapper/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { flag } = defineProps(["flag"]);
+</script>
+<template>
+  <div class="loading-wrapper-container">
+    <BaseLoading class="loading-container" v-if="flag" />
+    <slot v-else></slot>
+  </div>
+</template>
+<style scoped lang="scss">
+@use "./loadingWrapper.scss";
+</style>
