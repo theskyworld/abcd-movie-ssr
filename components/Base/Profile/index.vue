5016c9778db074b1fc2b0b7a556722b3c6b436aa
theskyworld <2456450687@qq.com>
2024-01-12 01:31:37 +0800

    feat: add Profile

---
+++ components/Base/Profile/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { ProfileProps } from "./types";
+const { imgURL } = defineProps<ProfileProps>();
+</script>
+<template>
+  <div class="profile-container">
+    <div class="img-wrapper">
+      <img :src="imgURL" alt="" />
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+@use "./profile.scss";
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,12 @@
 useHead({
   title: "ABCD影视",
 });
+const imgURL = ref("https://imgapi.xl0408.top/index.php");
 </script>
 <template>
   <div>首页</div>
   <div>
-    <Search />
+    <BaseProfile :imgURL="imgURL" />
   </div>
 </template>
 <style scoped></style>
