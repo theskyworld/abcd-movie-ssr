cfba0ab49a279b1cf987293f857cf2efadbcc19b
theskyworld <2456450687@qq.com>
2024-01-11 23:58:39 +0800

    chore: add icons for SearchAdvise

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script>
+useHead({
+  script: [
+    {
+      src: import("~/assets/fonts/icon-font.js"),
+    },
+  ],
+});
+</script>
 <template>
   <div class="app-container">
     <div class="dynamic-parts-wrapper">
