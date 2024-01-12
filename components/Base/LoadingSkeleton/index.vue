31bacd9214d2e500e0c017c0ed2584d18156a9ed
theskyworld <2456450687@qq.com>
2024-01-12 14:15:07 +0800

    feat: add LoadingSkeleton

---
+++ components/Base/LoadingSkeleton/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+// 决定要渲染的skeleton卡片的数量
+const cardCount = ref(12);
+</script>
+<template>
+  <div class="loading-skeleton-container">
+    <div
+      class="video-show-card-wrapper"
+      v-for="(i, index) in cardCount"
+      :key="index"
+    >
+      <div class="placeholder img">&nbsp;</div>
+      <div class="placeholder title">&nbsp;</div>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+@use "./loadingSkeleton.scss";
+</style>
